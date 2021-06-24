873149b5fb5d5b90c28afddb6296f717e2c63e48
Eduardo Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2021-06-24 11:40:52 -0300

    Estrutura

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,8 @@ const routes = require("express").Router();
 const dialplanController = require("./controller/dialplanController");
 const directoryController = require("./controller/directoryController");
 
-routes.get("/dialplan", dialplanController.index);
-routes.get("/directory", directoryController.index);
+routes.post("/dialplan", dialplanController.index);
+routes.post("/directory", directoryController.index);
 
 module.exports = routes;
 

1c56aad84b7b4115e7ddfff12c50c4924f1a24a5
Eduardo Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2021-06-25 17:58:09 -0300

    Dialplan to Register Gateway

--- src/controller/dialplanController.js
+++ src/controller/dialplanController.js
@@ -39,6 +39,32 @@ module.exports = {
     }
 
     //
+    // Chamadas externas
+    //
+    if (section === "dialplan" && isInteger(variable_sip_to_user)) {
+      const xmlText =
+        `
+        <document type="freeswitch/xml">
+          <section name="dialplan" description="RE Dial Plan For FreeSwitch">
+            <context name="${variable_domain_name}">
+              <extension name="${variable_user_name}-${variable_sip_to_user}">
+                <condition field="destination_number" expression="^(${variable_sip_to_user})$">
+                  <action application="bridge" data="{absolute_codec_string=^^:PCMU:PCMA}sofia/gateway/` +
+        "${register-gateway}" +
+        `/$1"/>
+                </condition>
+              </extension>
+            </context>
+          </section>
+        </document>
+        `;
+
+      let xml = jxon.stringToJs(xmlText);
+      res.set("Content-Type", "text/xml");
+      return res.send(jxon.jsToString(xml));
+    }
+
+    //
     // Chamadas para contextos internos destinadas a ramais
     //
     if (
@@ -70,29 +96,6 @@ module.exports = {
       query: req.query,
       headers: req.headers,
     });
-
-    if (section === "dialplan") {
-      const xmlText =
-        `
-        <document type="freeswitch/xml">
-          <section name="dialplan" description="RE Dial Plan For FreeSwitch">
-            <context name="${variable_domain_name}">
-              <extension name="${variable_user_name}-${variable_sip_to_user}">
-                <condition field="destination_number" expression="^(${variable_sip_to_user})$">
-                  <action application="bridge" data="{absolute_codec_string=^^:PCMU:PCMA}sofia/gateway/` +
-        "${register-gateway}" +
-        `/$1"/>
-                </condition>
-              </extension>
-            </context>
-          </section>
-        </document>
-        `;
-
-      let xml = jxon.stringToJs(xmlText);
-      res.set("Content-Type", "text/xml");
-      return res.send(jxon.jsToString(xml));
-    }
 
     res.set("Content-Type", "text/xml");
     return res.send("...Dialplan");

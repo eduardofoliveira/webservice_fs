e9660a3057f29a565999eff81b24d77bf53ffd44
Eduardo Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2021-06-25 11:16:45 -0300

    Configuration

---
+++ src/controller/configurationController.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async index(req, res) {
+    console.log({
+      body: req.body,
+      params: req.params,
+      query: req.query,
+      headers: req.headers,
+    });
+
+    res.send("...Configuration");
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ const routes = require("express").Router();
 
 const dialplanController = require("./controller/dialplanController");
 const directoryController = require("./controller/directoryController");
+const configurationController = require("./controller/configurationController");
 
 routes.post("/dialplan", dialplanController.index);
 routes.post("/directory", directoryController.index);
+routes.post("/configuration", configurationController.index);
 
 module.exports = routes;

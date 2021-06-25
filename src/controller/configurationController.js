e8346497aeae61507268267bf10f6ffdd1ff3c38
Eduardo Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2021-06-25 12:30:43 -0300

    Load Profile WebService

--- src/controller/configurationController.js
+++ src/controller/configurationController.js
@@ -5,15 +5,17 @@ module.exports = {
     const { section, profile } = req.body;
 
     if (section === "configuration" && profile === "internal") {
+      console.log("configuration - internal");
+
+      console.log({
+        body: req.body,
+        params: req.params,
+        query: req.query,
+        headers: req.headers,
+      });
+
       return res.send(await loadProfileTemplate());
     }
-
-    console.log({
-      body: req.body,
-      params: req.params,
-      query: req.query,
-      headers: req.headers,
-    });
 
     return res.send("...Configuration");
   },
